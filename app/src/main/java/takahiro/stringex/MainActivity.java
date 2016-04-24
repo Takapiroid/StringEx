5684579c6318e72a5a0942e576cca2f278b197d1
takahiro <takahiro.okawa1842@gmail.com>
2016-04-24 15:36:26 +0900

    first commit

---
+++ app/src/main/java/takahiro/stringex/MainActivity.java
@@ -0,0 +1,24 @@
+package takahiro.stringex;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new StringView(this));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
